1b405b85298aeb087f3b6702aeee6c489a55bc05
Daneille Boggs <daniboggs22@gmail.com>
2023-02-20 10:03:18 -0600

    Updated files to launchCode's to see why it is not running in browswer.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -10,23 +10,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Created by Chris Bay
+ */
 @Controller
 @RequestMapping("events")
 public class EventController {
-   List<String> events = new ArrayList<>();
-   @GetMapping
-    public String displayAllEvents(Model model){
-       model.addAttribute("events", events);
-       return "events/index";
 
+   private static List<String> events = new ArrayList<>();
+
+   @GetMapping
+   public String displayAllEvents(Model model) {
+      model.addAttribute("title", "All Events");
+      model.addAttribute("events", events);
+      return "events/index";
    }
+
    @GetMapping("create")
-   public String renderCreateEventForm() {
+   public String displayCreateEventForm(Model model) {
+      model.addAttribute("title", "Create Event");
       return "events/create";
    }
+
    @PostMapping("create")
-   public String createEvent(@RequestParam String eventName){
+   public String processCreateEventForm(@RequestParam String eventName) {
       events.add(eventName);
-      return ("redirect:");
+      return "redirect:";
    }
+
 }
